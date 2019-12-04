1eaad38304a10556a7404000d3172be04155f9a0
André Martins <andrebertonhadev@gmail.com>
2019-12-04 01:39:34 -0300

    react native animations bonus bootcamp

---
+++ App.js
@@ -0,0 +1,56 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {useEffect, useState} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Animated,
+  StatusBar,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+export default function App() {
+  const [ballY, setBallY] = useState(new Animated.Value(0));
+
+  useEffect(() => {
+    Animated.timing(ballY, {
+      toValue: 500,
+      duration: 1000,
+    }).start();
+  }, [ballY]);
+
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <View style={styles.container}>
+          <Animated.View style={[styles.ball, {top: ballY}]} />
+        </View>
+      </SafeAreaView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 30,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  ball: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    color: '#F00',
+  },
+});
